1b5531718ca585610cc3ead463e8034293425c73
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2020-10-13 15:37:41 -0300

    Passing product details dynamically

--- front-mobile/src/pages/Catalog/index.tsx
+++ front-mobile/src/pages/Catalog/index.tsx
@@ -16,49 +16,55 @@ import styles from "./styles";
 const Catalog = () => {
   const navigation = useNavigation();
   const [products] = useState([
-        {
-            title: 'Computador Desktop - Intel Core i7',
-            photo: require('../../images/product.jpg'),
-            price: '2.779,00',
-            description: 'Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.',
-            key: '1'
-        },
-        {
-            title: 'Computador Desktop - Intel Core i9',
-            photo: require('../../images/login.png'),
-            price: '3.779,00',
-            description: 'Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.',
-            key: '2'
-        },
-        {
-            title: 'Computador Desktop - Intel Core i3',
-            photo: require('../../images/iphone.png'),
-            price: '1.779,00',
-            description: 'Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.',
-            key: '3'
-        },
-        {
-            title: 'Computador Desktop - Intel Core i9',
-            photo: require('../../images/product.jpg'),
-            price: '3.779,00',
-            description: 'Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.',
-            key: '4'
-        },
-        {
-            title: 'Computador Desktop - Intel Core i9',
-            photo: require('../../images/main-image.png'),
-            price: '3.779,00',
-            description: 'Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.',
-            key: '5'
-        },
-        {
-            title: 'Computador Desktop - Intel Core i9',
-            photo: require('../../images/main-image.png'),
-            price: '3.779,00',
-            description: 'Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.',
-            key: '6'
-        },
-    ]);
+    {
+      title: "Computador Desktop - Intel Core i7",
+      photo: require("../../images/product.jpg"),
+      price: "2.779,00",
+      description:
+        "Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.",
+      key: "1",
+    },
+    {
+      title: "Computador Desktop - Intel Core i9",
+      photo: require("../../images/login.png"),
+      price: "3.779,00",
+      description:
+        "Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.",
+      key: "2",
+    },
+    {
+      title: "Computador Desktop - Intel Core i3",
+      photo: require("../../images/iphone.png"),
+      price: "1.779,00",
+      description:
+        "Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.",
+      key: "3",
+    },
+    {
+      title: "Computador Desktop - Intel Core i9",
+      photo: require("../../images/product.jpg"),
+      price: "3.779,00",
+      description:
+        "Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.",
+      key: "4",
+    },
+    {
+      title: "Computador Desktop - Intel Core i9",
+      photo: require("../../images/main-image.png"),
+      price: "3.779,00",
+      description:
+        "Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.",
+      key: "5",
+    },
+    {
+      title: "Computador Desktop - Intel Core i9",
+      photo: require("../../images/main-image.png"),
+      price: "3.779,00",
+      description:
+        "Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.",
+      key: "6",
+    },
+  ]);
 
   return (
     <>
@@ -126,28 +132,25 @@ const Catalog = () => {
       <FlatList
         style={{ marginTop: 270 }}
         data={products}
-        keyExtractor={item => item.key}
+        keyExtractor={(item) => item.key}
         numColumns={2}
         renderItem={({ item }) => (
-            <View style={styles.cardsContainer}>
-              <TouchableOpacity
-                onPress={() => navigation.navigate('ProductDetails', item)}
-                activeOpacity={0.5}
-                style={styles.button}
-              >
-                <Image source={item.photo} style={styles.productImageStyle} />
-                <Text style={styles.buttonText}>
-                  {item.title}
-                </Text>
-                <View style={styles.priceContainer}>
-                  <Text style={styles.currency}>R$</Text>
-                  <Text style={styles.price}>{item.price}</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-          )}
-        />
-       
+          <View style={styles.cardsContainer}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("ProductDetails", item)}
+              activeOpacity={0.5}
+              style={styles.button}
+            >
+              <Image source={item.photo} style={styles.productImageStyle} />
+              <Text style={styles.buttonText}>{item.title}</Text>
+              <View style={styles.priceContainer}>
+                <Text style={styles.currency}>R$</Text>
+                <Text style={styles.price}>{item.price}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </>
   );
 };
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ProductsResponse } from "../../core/types/Products";
-import { makeRequest } from '../../core/utils/request';
+import { makeRequest } from "../../core/utils/request";
 import ProductCard from "./components/ProductCard";
 import "./styles.scss";
 
@@ -11,24 +11,25 @@ const Catalog = () => {
   useEffect(() => {
     const params = {
       page: 0,
-      linesPerPage: 12
-    }
-    makeRequest({url: '/products', params})
-    .then(response => setProductsReponse(response.data));
+      linesPerPage: 12,
+    };
+    makeRequest({ url: "/products", params }).then((response) =>
+      setProductsReponse(response.data)
+    );
   }, []);
 
-  return(
+  return (
     <div className='catalog-container'>
-    <h1 className='catalog-title'>Catálogo de Produtos</h1>
-    <div className='catalog-products'>
-      {productsResponse?.content.map(product => (
-        <Link to='/products/1' key={product.id}>
-          <ProductCard product={product}/>
-        </Link>
-      ))}
+      <h1 className='catalog-title'>Catálogo de Produtos</h1>
+      <div className='catalog-products'>
+        {productsResponse?.content.map((product) => (
+          <Link to={`/products/${product.id}`} key={product.id}>
+            <ProductCard product={product} />
+          </Link>
+        ))}
+      </div>
     </div>
-  </div>
   );
-}
+};
 
 export default Catalog;
