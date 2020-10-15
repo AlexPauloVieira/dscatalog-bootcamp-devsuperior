eb3694320e2e12fe482a05ada939c26dfd8cc4bc
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2020-10-15 18:43:19 -0300

    Finish Catalog loaders

--- front-web/src/pages/Catalog/components/ProductCard/index.tsx
+++ front-web/src/pages/Catalog/components/ProductCard/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-import ProducPrice from "../../../../core/components/ProductPrice";
-import { Product } from "../../../../core/types/Products";
+import ProducPrice from "core/components/ProductPrice";
+import { Product } from "core/types/Products";
 import "./styles.scss";
 
 type Props = {
   product: Product;
-}
+};
 
 const ProductCard = ({ product }: Props) => (
   <div className='card-base border-radius-10 product-card'>
-    <img src={product.imgUrl} alt={product.name} className="product-card-image"/>
+    <img
+      src={product.imgUrl}
+      alt={product.name}
+      className='product-card-image'
+    />
     <div className='product-info'>
       <h6 className='product-name'>{product.name}</h6>
       <ProducPrice price={product.price} />
