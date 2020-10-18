4dba596a6632e8d44d879f6d122ef347c90f2e99
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2020-10-17 23:34:03 -0300

    Base Admin Route Structure

--- front-web/src/pages/Admin/components/Navbar/index.tsx
+++ front-web/src/pages/Admin/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import "./styles.scss";
 
@@ -6,19 +7,19 @@ const Navbar = () => (
   <nav className='admin-nav-container'>
     <ul>
       <li>
-        <a href='link' className='admin-nav-item active'>
+        <NavLink to='/admin/products' className='admin-nav-item'>
           Meus Produtos
-        </a>
+        </NavLink>
       </li>
       <li>
-        <a href='link' className='admin-nav-item'>
+        <NavLink to='/admin/categories' className='admin-nav-item'>
           Minhas Categorias
-        </a>
+        </NavLink>
       </li>
       <li>
-        <a href='link' className='admin-nav-item'>
+        <NavLink to='/admin/users' className='admin-nav-item'>
           Meus Usuários
-        </a>
+        </NavLink>
       </li>
     </ul>
   </nav>
