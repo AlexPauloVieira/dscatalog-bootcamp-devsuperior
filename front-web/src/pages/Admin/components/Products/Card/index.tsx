45f2e27eb3979a2ba0fa1e433fcaf16d7b228792
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2020-11-24 15:58:43 -0300

    Integrate product listing with backend

--- front-web/src/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -1,32 +1,32 @@
 import ProducPrice from "core/components/ProductPrice";
+import { Product } from "core/types/Products";
 import React from "react";
 import "./styles.scss";
 
-const Card = () => {
+type Props = {
+  product: Product;
+};
+
+const Card = ({ product }: Props) => {
   return (
     <div className='card-base product-card-admin'>
       <div className='row'>
         <div className='col-2 text-center border-right py-3'>
           <img
-            src='https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg'
-            alt='Produto Teste'
+            src={product.imgUrl}
+            alt={product.name}
             className='product-card-image-admin'
           />
         </div>
         <div className='col-7 py-3'>
-          <h3 className='product-card-name-admin'>
-            Computador Desktop - Intel Core i7
-          </h3>
-          <ProducPrice price={40.5} />
+          <h3 className='product-card-name-admin'>{product.name}</h3>
+          <ProducPrice price={product.price} />
           <div>
             <span className='badge badge-pill badge-secondary mr-2'>
               Categoria 1
             </span>
             <span className='badge badge-pill badge-secondary mr-2'>
               Categoria 2
-            </span>
-            <span className='badge badge-pill badge-secondary mr-2'>
-              Categoria 3
             </span>
           </div>
         </div>
